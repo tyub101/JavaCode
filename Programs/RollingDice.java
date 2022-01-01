8aadb8425ee263fadf47434e2a5ede26a4ab38ae
ABDELILAH AIT AHMED OUHAMOU <96702843+tyub101@users.noreply.github.com>
2022-01-01 16:12:12 +0100

    Create RollingDice.java

---
+++ Programs/RollingDice.java
@@ -0,0 +1,14 @@
+
+import java.util.Random;
+
+public class RollingDice {
+
+	public static void main(String[] args) {
+
+		Random rand = new Random();
+		int result = rand.nextInt(6) + 1;
+		System.out.println("You rolled a: " + result);
+
+	}
+
+}
